e9758b1b30319bd54c5026a5fad50e9cfea11dca
JokerRoller <BECOOL686868@gmail.com>
2022-05-20 05:42:37 +0400

    asd

--- Assets/Js.js
+++ Assets/Js.js
@@ -1,5 +1,67 @@
 window.addEventListener('scroll', function () {
     let header = document.querySelector('nav');
-    let windowPosition = window.scrollY > 50;
+    let windowPosition = window.scrollY > 100;
     header.classList.toggle('scrolling-active', windowPosition);
 })
+
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js1');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone', windowPosition2);
+})
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js2');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone', windowPosition2);
+})
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js3');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone', windowPosition2);
+})
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js4');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone', windowPosition2);
+})
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js5');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone2', windowPosition2);
+})
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js6');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone2', windowPosition2);
+})
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js7');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone2', windowPosition2);
+})
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js8');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone2', windowPosition2);
+})
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js9');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone3', windowPosition2);
+})
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js10');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone3', windowPosition2);
+})
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js11');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone3', windowPosition2);
+})
+window.addEventListener('scroll', function () {
+    let header2 = document.querySelector('.js12');
+    let windowPosition2 = window.scrollY > 10;
+    header2.classList.toggle('anotherone3', windowPosition2);
+})
+
